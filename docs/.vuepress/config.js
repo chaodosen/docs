c13d3916a7c31b78480d8738983ff5f4275d14a8
coderzds <coderzds@163.com>
2022-11-16 21:45:18 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,44 @@
+const moment = require('moment')
+moment.locale("zh-cn")
+module.exports = {
+    base: "/docs/",
+    title: "石头屋",
+    description: "太平洋比基尼海滩比奇堡贝克街126号石头下面",
+    head: [
+        ['link',{rel: 'icon',href: '/assets/img/icon.ico'}],
+        ['meta',{name: 'author',content: 'coderzds'}],
+        ['meta',{name: 'Keywords',content: '海绵宝宝,派大星'}],
+    ],
+    plugins: [
+        [
+          '@vuepress/last-updated',
+          {
+            transformer: (timestamp) => {
+              return moment(timestamp).calendar()
+            }
+          }
+        ]
+      ],
+    themeConfig: {
+        logo: '/assets/img/icon.ico',
+        lastUpdated: '最近更新',
+        nav: [
+            {text: 'Home',link: '/'},
+            {text: 'about',link: '/about/'},
+            {
+              text: 'blogs',
+              items: [
+                {
+                  text: 'test',link: '/blogs/2022/test'
+                }
+              ]
+            }
+        ],
+        sidebar: {
+            "/blogs/2022/": [
+                'test',
+                'test2'
+            ]
+        }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroImage: /assets/img/favicon.ico
+heroText: 石头屋
+tagline: 太平洋比基尼海滩比奇堡贝克街126号石头下面
+actionText: 快速传送 →
+actionLink: /guide/
+---
