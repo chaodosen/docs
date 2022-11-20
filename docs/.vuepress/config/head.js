508b1c659ae9215cf3e8c67cb6129575af24970c
coderzds <coderzds@163.com>
2022-11-20 18:12:05 +0800

    add pwa plugin

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -2,4 +2,12 @@ module.exports = [
     ['link',{rel: 'icon',href: '/assets/img/icon.ico'}],
     ['meta',{name: 'author',content: 'coderzds'}],
     ['meta',{name: 'Keywords',content: '海绵宝宝,派大星'}],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
 ]
--- docs/.vuepress/config/plugin.js
+++ docs/.vuepress/config/plugin.js
@@ -1,11 +1,18 @@
 const moment = require('moment')
 moment.locale("zh-cn")
 module.exports = {
-    '@vuepress/last-updated': {
-        transformer: (timestamp) => {
-          return moment(timestamp).format("l HH:mm:ss dddd")
-        }
-    },
-    '@vuepress/back-to-top': true,
-    '@vuepress/google-analytics': require('./google-analytics')
+  '@vuepress/last-updated': {
+    transformer: (timestamp) => {
+      return moment(timestamp).format("l HH:mm:ss dddd")
+    }
+  },
+  '@vuepress/back-to-top': true,
+  '@vuepress/google-analytics': require('./google-analytics'),
+  '@vuepress/pwa': {
+    serviceWorker: true,
+    updatePopup: {
+      message: "发现新内容可用",
+      buttonText: "刷新"
+    }
+  }
 }
