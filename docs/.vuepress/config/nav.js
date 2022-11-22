1913fefa74ee9e10f1d9615de813b0ece915cc92
coderzds <coderzds@163.com>
2022-11-23 01:37:43 +0800

    add icons in nav

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,7 +1,38 @@
 module.exports = [
-    {text: '首页',link: '/'},
-    {text: 'Linux专栏',link: '/Linux/'},
-    {text: '面试题',link: '/interview/Java'},
-    {text: 'BUG',link: '/bugs/prob-in-work/method-breakpoints'},
-    {text: '关于我',link: '/about/'},
+    {text: '首页',link: '/', icon: 'reco-home'},
+    {text: 'Linux专栏',link: '/Linux/',icon: 'iconfont icon-centos'},
+    {text: 'BUG',link: '/bugs/prob-in-work/method-breakpoints',icon: 'iconfont icon-bug'},
+    {text: '面试题',link: '/interview/Java',icon: 'iconfont icon-mianshi'},
+    {
+        text:'计划',
+        icon: 'iconfont icon-schedule',
+        items: [
+            {
+                text: '电影',
+                icon: 'iconfont icon-movie',
+                link: '/schedule/movies/to-be-seen'
+            }
+        ]
+    },
+    {
+        text: '联系我',
+        icon: 'reco-account',
+        items: [
+            {
+                text: '关于我',
+                link: '/about/',
+                icon: 'iconfont icon-wowowo'
+            },
+            {
+                text: 'Github',
+                link: 'https://github.com/coderzds',
+                icon: 'reco-github'
+            },
+            {
+                text: 'CSDN',
+                link: 'https://blog.csdn.net/coderzds',
+                icon: 'reco-csdn'
+            },
+        ]
+    },
 ]
