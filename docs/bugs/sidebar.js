a5d0bd7f53bd677f98694ae0f0ec9c753cbc8733
coderzds <coderzds@163.com>
2022-11-23 16:55:20 +0800

    add bug of front-end

--- docs/bugs/sidebar.js
+++ docs/bugs/sidebar.js
@@ -3,7 +3,8 @@ module.exports = [
         title: '工作时遇到的bug',
         collapsable: false,
         children: [
-            '/bugs/prob-in-work/method-breakpoints'
+            '/bugs/prob-in-work/method-breakpoints',
+            '/bugs/prob-in-work/npm-taobao-mirror-delay'
         ]
     }
 ]
